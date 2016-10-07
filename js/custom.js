24f007a2dcb5c340ac44ddaa89b6d68d23ffb89a
smriti <iamsmritid@gmail.com>
2016-10-07 15:56:00 +0530

    All changes done except header

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,62 @@
 $(document).ready(function(){
-    $(window).bind('scroll', function() {
-        var navHeight = 300; // custom nav height
-        ($(window).scrollTop() > navHeight) ? $('nav').addClass('goToTop') : $('nav').removeClass('goToTop');
-    }); 
+    // $(window).bind('scroll', function() {
+    //     var navHeight = 300; // custom nav height
+    //     ($(window).scrollTop() > navHeight) ? $('nav').addClass('goToTop') : $('nav').removeClass('goToTop');
+    // });
+
+    // $(window).scroll(function () {
+    //     //if you hard code, then use console
+    //     //.log to determine when you want the
+    //     //nav bar to stick.
+    //     console.log($(window).scrollTop());
+    //     if ($(window).scrollTop() > 46) {
+    //         $('.navbar').addClass('navbar-fixed-top');
+    //     }
+    //     if ($(window).scrollTop() < 47) {
+    //         $('.navbar').removeClass('navbar-fixed-top');
+    //     }
+    // });
+    // var MQL = 1170;
+
+    //primary navigation slide-in effect
+    // if($(window).width() > MQL) {
+    //     var headerHeight = $('.navbar').height();
+    //     $(window).on('scroll',
+    //         {
+    //             previousTop: 0
+    //         },
+    //         function () {
+    //             var currentTop = $(window).scrollTop();
+    //             //check if user is scrolling up
+    //             if (currentTop < this.previousTop ) {
+    //                 //if scrolling up...
+    //                 if (currentTop > 0 && $('.navbar').hasClass('is-fixed')) {
+    //                     $('.cd-header').addClass('is-visible');
+    //                 } else {
+    //                     $('.cd-header').removeClass('is-visible is-fixed');
+    //                 }
+    //             } else {
+    //                 //if scrolling down...
+    //                 $('.cd-header').removeClass('is-visible');
+    //                 if( currentTop > headerHeight && !$('.cd-header').hasClass('is-fixed')) $('.cd-header').addClass('is-fixed');
+    //             }
+    //             this.previousTop = currentTop;
+    //         });
+    // }
+
+
+
+
+    $(window).bind('scroll', function () {
+        if ($(window).scrollTop() > 100) {
+            $('.navbar').removeClass('m-t-46');
+        } else {
+            $('.navbar').addClass('navbar-fixed-top');
+        }
+    });
+
+           // if($(".navbar").hasClass("m-t-46")) {
+        //     $(this).removeClass("m-t-46");
+        // }
 });
 
