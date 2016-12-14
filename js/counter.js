8b01f9053f1cd29779c73665520c2a861ed426f5
garima <garima8jun@gmail.com>
2016-12-14 13:31:18 +0530

    one section added

--- js/counter.js
+++ js/counter.js
@@ -4,7 +4,7 @@ function countUp(count)
         speed = Math.round(count / div_by),
         $display = $('.count'),
         run_count = 1,
-        int_speed = 24;
+        int_speed = 12;
 
     var int = setInterval(function() {
         if(run_count < div_by){
@@ -19,8 +19,21 @@ function countUp(count)
     }, int_speed);
 }
 
-// countUp(1209);
+/*
+ // countUp(1209);
 
-$("#counters").mouseover(function () {
-    countUp(1209);
-})
+ $("#counters").mouseov(function () {
+ countUp(1209);
+ })
+ */
+
+var x = true;
+$(window).scroll(startCounter);
+function startCounter() {
+    if ($(window).scrollTop() > 3200) {
+        if (x==true){
+            countUp(1209);
+            x=false;
+        }
+    };
+}
